575421a10b00eab730383d39c048f324bd52efa3
Aliaksandr Ihnatsiuk <a.ihnatsiuk@digital-science.com>
2022-05-10 15:40:39 +0300

    Drop pipeline

--- ronald13-project/ml_project/ml_project/train.py
+++ ronald13-project/ml_project/ml_project/train.py
@@ -4,16 +4,6 @@ import click
 import mlflow
 import mlflow.sklearn
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import KFold
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import MinMaxScaler
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import GridSearchCV
-from sklearn.model_selection import cross_validate
-
 from .data import get_dataset
 
 
@@ -21,7 +11,7 @@ from .data import get_dataset
 @click.option(
     "-d",
     "--dataset-path",
-    default="data/train.csv",
+    default="data/heart.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
     show_default=True,
 )
@@ -39,32 +29,20 @@ from .data import get_dataset
     show_default=True,
 )
 @click.option(
-    "--search",
-    default="NestedCV",
-    type=click.Choice(["KFold", "NestedCV"]),
-    show_default=True,
-)
-@click.option(
-    "--select-model",
-    default="logist_regression",
-    type=click.Choice(["logist_regression", "random_forest"]),
-    show_default=True,
-)
-@click.option(
     "--test-split-ratio",
     default=0.2,
     type=click.FloatRange(0, 1, min_open=True, max_open=True),
     show_default=True,
 )
 @click.option(
-    "--feature-engineering",
-    default="standard_scaler",
-    type=click.Choice(["standard_scaler", "min_max_scaler", "None"]),
+    "--use-scaler",
+    default=True,
+    type=bool,
     show_default=True,
 )
 @click.option(
     "--max-iter",
-    default=500,
+    default=100,
     type=int,
     show_default=True,
 )
@@ -74,21 +52,26 @@ from .data import get_dataset
     type=float,
     show_default=True,
 )
-@click.option(
-    "--n-estimators",
-    default=100,
-    type=int,
-    show_default=True,
-)
-@click.option(
-    "--max-depth",
-    default=None,
-    type=int,
-    show_default=True,
-)
-@click.option(
-    "--max-features",
-    default=1,
-    type=click.FloatRange(0, 1, min_open=True, max_open=False),
-    show_default=True,
-)
+def train(
+    dataset_path: Path,
+    save_model_path: Path,
+    random_state: int,
+    test_split_ratio: float,
+    use_scaler: bool,
+    max_iter: int,
+    logreg_c: float,
+) -> None:
+    features_train, features_val, target_train, target_val = get_dataset(
+        dataset_path,
+        random_state,
+        test_split_ratio,
+    )
+    with mlflow.start_run():
+
+        #accuracy = accuracy_score(target_val, pipeline.predict(features_val))
+        mlflow.log_param("use_scaler", use_scaler)
+        mlflow.log_param("max_iter", max_iter)
+        mlflow.log_param("logreg_c", logreg_c)
+        mlflow.log_metric("accuracy", accuracy)
+        click.echo(f"Accuracy: {accuracy}.")
+        click.echo(f"Model is saved to {save_model_path}.")
