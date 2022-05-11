c454761688a73947988c85efbb77336b04ce8a99
Aliaksandr Ihnatsiuk <a.ihnatsiuk@digital-science.com>
2022-05-11 10:53:12 +0300

    Add new parament and rename file

--- ronald13-project/ml_project/ml_project/train.py
+++ ronald13-project/ml_project/ml_project/train.py
@@ -11,7 +11,7 @@ from .data import get_dataset
 @click.option(
     "-d",
     "--dataset-path",
-    default="data/heart.csv",
+    default="data/train.csv",
     type=click.Path(exists=True, dir_okay=False, path_type=Path),
     show_default=True,
 )
@@ -29,6 +29,18 @@ from .data import get_dataset
     show_default=True,
 )
 @click.option(
+    "--search",
+    default="NestedCV",
+    type=click.Choice(["KFold", "NestedCV"]),
+    show_default=True,
+)
+@click.option(
+    "--select-model",
+    default="logist_regression",
+    type=click.Choice(["logist_regression", "random_forest"]),
+    show_default=True,
+)
+@click.option(
     "--test-split-ratio",
     default=0.2,
     type=click.FloatRange(0, 1, min_open=True, max_open=True),
@@ -42,7 +54,7 @@ from .data import get_dataset
 )
 @click.option(
     "--max-iter",
-    default=100,
+    default=500,
     type=int,
     show_default=True,
 )
@@ -52,16 +64,40 @@ from .data import get_dataset
     type=float,
     show_default=True,
 )
+@click.option(
+    "--n-estimators",
+    default=100,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--max-depth",
+    default=None,
+    type=int,
+    show_default=True,
+)
+@click.option(
+    "--max-features",
+    default=1,
+    type=click.FloatRange(0, 1, min_open=True, max_open=False),
+    show_default=True,
+)
+
 def train(
     dataset_path: Path,
     save_model_path: Path,
     random_state: int,
     test_split_ratio: float,
-    use_scaler: bool,
+    feature_engineering: str,
     max_iter: int,
     logreg_c: float,
+    select_model: str,
+    n_estimators: int,
+    max_depth: int,
+    max_features: int,
+    search: str,
 ) -> None:
-    features_train, features_val, target_train, target_val = get_dataset(
+    features_train, target_train = get_dataset(
         dataset_path,
         random_state,
         test_split_ratio,
