1234458097cc61fff8b5315238168a0413a08566
Aliaksandr Ihnatsiuk <a.ihnatsiuk@digital-science.com>
2022-05-11 18:24:23 +0300

    add flake8 recomendation

--- ronald13-project/ml_project/ml_project/data.py
+++ ronald13-project/ml_project/ml_project/data.py
@@ -1,11 +1,7 @@
-import logging
 from pathlib import Path
 from typing import Tuple
-
 import click
 import pandas as pd
-from sklearn.model_selection import train_test_split
-
 
 def get_dataset(
         csv_path: Path, random_state: int, test_split_ratio: float
@@ -14,5 +10,4 @@ def get_dataset(
     click.echo(f"Dataset shape: {dataset.shape}.")
     target_train = dataset["Cover_Type"]
     features_train = dataset.drop("Cover_Type", axis=1)
-
     return features_train, target_train
