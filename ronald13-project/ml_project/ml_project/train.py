a5eb7ce84987088f59e617f7068f3869112bc488
Aliaksandr Ihnatsiuk <a.ihnatsiuk@digital-science.com>
2022-05-11 12:21:25 +0300

    Add LogisticRegression

--- ronald13-project/ml_project/ml_project/train.py
+++ ronald13-project/ml_project/ml_project/train.py
@@ -82,7 +82,12 @@ from .data import get_dataset
     type=click.FloatRange(0, 1, min_open=True, max_open=False),
     show_default=True,
 )
-
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import cross_validate
 def train(
     dataset_path: Path,
     save_model_path: Path,
@@ -92,22 +97,69 @@ def train(
     max_iter: int,
     logreg_c: float,
     select_model: str,
-    n_estimators: int,
-    max_depth: int,
-    max_features: int,
+
     search: str,
 ) -> None:
     features_train, target_train = get_dataset(
         dataset_path,
         random_state,
-        test_split_ratio,
+        test_split_ratio
     )
     with mlflow.start_run():
 
-        #accuracy = accuracy_score(target_val, pipeline.predict(features_val))
-        mlflow.log_param("use_scaler", use_scaler)
-        mlflow.log_param("max_iter", max_iter)
-        mlflow.log_param("logreg_c", logreg_c)
+        cv = KFold(n_splits=8, shuffle=True, random_state=random_state)
+
+        if search == "KFold":
+            model = LogisticRegression(
+                random_state=random_state, max_iter=max_iter, C=logreg_c, n_jobs=-1)
+            scores = cross_validate(
+                model,
+                features_train,
+                target_train,
+                cv=cv,
+                scoring=("accuracy", "roc_auc_ovr"),
+            )
+            accuracy = scores["test_accuracy"].mean()
+            roc_auc_ovr = scores["test_roc_auc_ovr"].mean()
+
+
+            mlflow.log_param("max_iter", max_iter)
+            mlflow.log_param("logreg_c", logreg_c)
+
+        elif search == "NestedCV":
+            model = LogisticRegression(random_state=random_state, n_jobs=-1)
+            space = dict()
+            space["max_iter"] = [10, 100, 200, 400, 500, 700]
+            space["C"] = [0.001, 0.01, 0.1, 1, 10]
+
+            search_q = GridSearchCV(model, space, scoring="accuracy", n_jobs=-1, cv=cv)
+            cv_outer = KFold(n_splits=5)
+            scores = cross_validate(
+                search_q,
+                features_train,
+                target_train,
+                cv=cv_outer,
+                scoring=("accuracy", "roc_auc_ovr"),
+            )
+            accuracy = scores["test_accuracy"].mean()
+            roc_auc_ovr = scores["test_roc_auc_ovr"].mean()
+
+            search_q.fit(features_train, target_train)
+            print(
+                "The parameters combination that would give best accuracy is : ",
+                search_q.best_params_,
+            )
+
+            mlflow.log_param("max_iter", search_q.best_params_["max_iter"])
+            mlflow.log_param("logreg_c", search_q.best_params_["C"])
+
+        mlflow.log_param("feature_engineering", feature_engineering)
+        mlflow.log_param("select_model", select_model)
+        mlflow.log_param("search", search)
         mlflow.log_metric("accuracy", accuracy)
+        mlflow.log_metric("roc_auc_ovr", roc_auc_ovr)
         click.echo(f"Accuracy: {accuracy}.")
+        click.echo(f"roc_auc_ovr: {roc_auc_ovr}.")
+        click.echo(f"select_model: {select_model}.")
+        dump(model, save_model_path)
         click.echo(f"Model is saved to {save_model_path}.")
